1568bddb111447d3d0a85c9d6da28cd71fa3bc6d
Hardik Mishra <hardik.s.mishra@slrtce.in>
2026-09-19 18:47:27 +0530

    File upload size increased from 25Mb to 50Mb

--- project/server/src/middleware/upload.js
+++ project/server/src/middleware/upload.js
@@ -40,7 +40,7 @@ const fileFilter = (req, file, cb) => {
 const upload = multer({
   storage,
   fileFilter,
-  limits: { fileSize: 25 * 1024 * 1024 }, // 25MB
+  limits: { fileSize: 50 * 1024 * 1024 }, // 25MB
 });
 
 module.exports = { upload, uploadDir };
